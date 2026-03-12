ed1bd391ed9783882f9495b28876d193527f4303
Julian Lechuga <julianlechuga12@gmail.com>
2026-03-12 01:08:00 +0100

    Fix router basename and build script

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const ScrollToTop = () => {
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Navbar />
       <main>
